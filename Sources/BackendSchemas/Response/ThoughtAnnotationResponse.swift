53535469a301c5ce8d8b98c2e9cdef8d6bced713
Dennis Müller <d3mueller@me.com>
2024-08-22 20:22:36 +0200

    Update Thought Annotation Response

--- Sources/BackendSchemas/Response/ThoughtAnnotationResponse.swift
+++ Sources/BackendSchemas/Response/ThoughtAnnotationResponse.swift
@@ -2,8 +2,13 @@ import Foundation
 
 public struct ThoughtAnnotationResponse: Codable, Sendable {
   public var annotatedThought: AnnotatedThoughtSchema
-
-  public init(annotatedThought: AnnotatedThoughtSchema) {
+  public var associatedThoughtIds: [String]
+  
+  public init(
+    annotatedThought: AnnotatedThoughtSchema,
+    associatedThoughtIds: [String]
+  ) {
     self.annotatedThought = annotatedThought
+    self.associatedThoughtIds = associatedThoughtIds
   }
 }
