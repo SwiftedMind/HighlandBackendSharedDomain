2b4123aa03aa0606fa9189b237d7142aa103a756
Dennis Müller <d3mueller@me.com>
2024-08-29 20:16:27 +0200

    Update Event

--- Sources/SharedModels/Event.swift
+++ Sources/SharedModels/Event.swift
@@ -1,11 +1,13 @@
 import Foundation
 
-public struct Event: Codable, Sendable, Hashable {
+public struct Event: Identifiable, Codable, Sendable, Hashable {
+  public var id: UUID
   public var name: String
   public var allDay: Bool
   public var date: Date
 
-  public init(name: String, allDay: Bool, date: Date) {
+  public init(id: UUID = .init(), name: String, allDay: Bool, date: Date) {
+    self.id = id
     self.name = name
     self.allDay = allDay
     self.date = date
