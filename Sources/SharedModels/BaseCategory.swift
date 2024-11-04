a9dc59d34e34565c8e00b1a61b572d834edd0212
Dennis Müller <d3mueller@me.com>
2024-11-04 16:18:37 +0100

    Fix

--- Sources/SharedModels/BaseCategory.swift
+++ Sources/SharedModels/BaseCategory.swift
@@ -1,5 +1,6 @@
 /// Prefixed to avoid conflicts (and random crashes) with a `Category` typedef in the Objective-C runtime.
-public enum BaseCategory: String, Codable, Sendable, Hashable, CaseIterable {
+public enum BaseCategory: String, Identifiable, Codable, Sendable, Hashable, CaseIterable {
+  public var id: Self { self }
   case note
   case reference
   case task
